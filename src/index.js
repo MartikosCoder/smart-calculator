0ea90728ce8f96b0fbad0d597f050c81a789636f
Martin <martbelmoaw@gmail.com>
2018-09-12 21:09:29 +0300

    ended

--- src/index.js
+++ src/index.js
@@ -1,75 +1,48 @@
 class SmartCalculator {
-  constructor(initValue) {
-    this.init = initValue;
-    this.operations = [];
-  }
-  get result() {
-    let rpn = [this.init]; // reverse polish notation
-    let ops = [];
-
-    this.operations.forEach(op => { // сортировочная станция
-      while (ops.length && (ops[ops.length - 1]).priority < op.priority) {
-        rpn.push(ops.pop().op);
-      }
-      ops.push(op);
-      rpn.push(op.val);
-    });
-    while (ops.length) {
-      rpn.push(ops.pop().op)
+  constructor(initialValue) {
+        this.value = initialValue.toString();
+        this.expression = "";
+        this.valueUsed = false;
     }
-    let res = [];
-    rpn.forEach(token => {
-      if (typeof token === 'function') {
-        res.push(token(res.pop(), res.pop()));
-      } else {
-        res.push(token);
-      }
-    });
-    return res.pop();
-  }
-  add(val) {
-    this.operations.push({
-      op: (a, b) => a + b,
-      priority: 2,
-      val: val
-    });
-    return this;
-  }
-  subtract(val) {
-    this.operations.push({
-      op: (a, b) => b - a,
-      priority: 2,
-      val: val
-    })
-    return this;
-  }
-  multiply(val) {
-    this.operations.push({
-      op: (a, b) => a * b,
-      priority: 1,
-      val: val
-    })
-    return this;
-  }
-  devide(val) {
-    this.operations.push({
-      op: (a, b) => b / a,
-      priority: 1,
-      val: val
-    })
-    return this;
-  }
-  pow(val) {
-    this.operations.push({
-      op: (a, b) => Math.pow(b, a),
-      priority: 1,
-      val: val
-    })
-    return this;
-  }
-  valueOf() {
-    return this.result;
-  }
+
+    add(operand) {
+        this.expression += `${this.useExpOrValue(operand)} + ${operand}`;
+        return this;
+    }
+
+    multiply(operand) {
+        this.expression += `${this.useExpOrValue(operand)} * ${operand}`;
+        return this;
+    }
+
+    subtract(operand) {
+        this.expression += `${this.useExpOrValue(operand)} - ${operand}`;
+        return this;
+    }
+
+    devide(operand) {
+        this.expression += `${this.useExpOrValue(operand)} / ${operand}`;
+        return this;
+    }
+
+    pow(operand) {
+        this.expression += `${this.useExpOrValue(operand)} ** ${operand}`;
+        return this;
+    }
+
+    useExpOrValue(operand) {
+        let res = !this.valueUsed ? this.value : "";
+        this.valueUsed = true;
+        return res;
+    }
+
+    get result() {
+        return eval(this.expression);
+    }
+
+    valueOf() {
+      return this.result;
+    }
 }
 
 module.exports = SmartCalculator;
